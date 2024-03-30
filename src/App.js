295018990f70b607e13f789ca2b533e803128d0f
Adityavyshnav-2001 <codeadityavyshnav@gmail.com>
2024-03-30 05:47:54 +0530

    Basics

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import "./App.css";
-
-function App() {
-  return <div className="text-3xl font-bold text-blue-400">Lets Build 2</div>;
-}
-
-export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import myimage from "../utils/Logo.png";
+
+const Header = () => {
+  return (
+    <div>
+        <img className="w-28" src={myimage} />
+        </div>
+  )
+}
+
+export default Header;
