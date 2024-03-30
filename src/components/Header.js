933549c9da1cad0748803e957d15d051a95a49b1
Adityavyshnav-2001 <codeadityavyshnav@gmail.com>
2024-03-30 14:43:00 +0530

    new

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 import myimage from "../utils/Logo.png";
 
 const Header = () => {
   return (
     <div>
-        <img className="w-28" src={myimage} />
-        </div>
-  )
-}
+      <img className="w-28" src={myimage} />
+    </div>
+  );
+};
 
 export default Header;
